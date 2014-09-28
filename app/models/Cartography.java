750ccf8f1f6e40df87550cd7a751502798229830
wisthler <wisthler@gmail.com>
2014-09-28 13:11:30 +0200

    WIP: preparing a first try of the cartography

---
+++ app/models/Cartography.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package models;
+
+import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author wisthler
+ *
+ */
+public class Cartography {
+	private static final Logger LOG = LoggerFactory.getLogger(Cartography.class);
+	
+	private SortedSet<Portal> allPortals = new TreeSet<>();
+	private SortedSet<Link> allLinks = new TreeSet<>();
+	
+	
+	
+	/* =============== Constructors              =============== */
+	
+	/**
+	 * create a basic cartography providing a list a portals
+	 * @param portals
+	 */
+	public Cartography(Portal... portals){
+		this.allPortals.addAll(Arrays.asList(portals));
+	}
+	
+	
+	
+	/* =============== Getters/Setters           =============== */
+	/* =============== Override/Implement method =============== */	
+	/* =============== Others methods            =============== */
+	
+	public void generateAllLinks(){
+		for(Portal a: allPortals){
+			for(Portal b: allPortals){
+				if(a != null && b != null && a.compareTo(b) < 0){
+					this.allLinks.add(new Link(a, b));
+					this.allLinks.add(new Link(b, a));
+				}
+			}
+		}
+	}
+}
---
+++ test/models/SimpleCartographyTest.java
@@ -0,0 +1,21 @@
+package models;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import play.test.UnitTest;
+
+public class SimpleCartographyTest extends UnitTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
